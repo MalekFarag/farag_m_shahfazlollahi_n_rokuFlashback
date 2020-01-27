527a345462783b6c129fc82d885d4d465b0021e7
Malek F <43182938+MalekFarag@users.noreply.github.com>
2020-01-27 13:24:10 -0500

    vue method fixed, components created, scroll list template created

--- js/main.js
+++ js/main.js
@@ -1,21 +1,38 @@
 
-window.onload = function(){
-    // Vue for topNav
+
+    // creating template for rendering media (music + film)
+    Vue.component('mediaContent',{
+        template: `
+            
+        `
+    })
+
+    // Vue for app
     var vm = new Vue({
-        el: '#topNav',
+        el: '#app',
 
         data:{
             user:{
                 isMusic: true
             },
-        
+
+            music:{
+                years: ['50s', '60s', '70s', '80s', '90s'],
+                genres: ['Rock', 'Pop', 'Folk', 'Hip-Hop', 'Metal']
+            },
+
+            film:{
+                years: ['50s', '60s', '70s', '80s', '90s'],
+                genres: ['Drama', 'Reality', 'Horror', 'Mystery', 'Sci-fi']
+            }
+
+        },
+
         methods:{
             userMedia(){
                 console.log('chaning media.');
                 this.user.isMusic = (this.user.isMusic) ? false : true;
             }
         }
-    }
     });
-}
 
